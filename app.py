735b8784815237216dd1640403b88e54c67011b0
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-08 13:20:38 -0700

    idk

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import streamlit as st
 def login_screen():
     st.header("This app is private.")
     st.subheader("Please log in.")
-    if st.button("Log in with Google"):
-        st.login()
+    st.button("Log in with Google", on_click=st.login)
 
-if not st.user.get('is_logged_in'):
+if not st.user.is_logged_in:
     login_screen()
 else:
-    print(st.user.to_dict())
     st.header(f"Welcome, {st.user.name}!")
     st.button("Log out", on_click=st.logout)
