2feae89018b010e3d93db8796955da8bf3c5b697
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-08 14:10:27 -0700

    scratch

--- app.py
+++ app.py
@@ -6,23 +6,50 @@ from router import route
 import os 
 import json 
 
-st.set_page_config(layout="wide")
-_CONFIG = json.load(open('config.json'))
+import streamlit as st
+from streamlit_google_auth import Authenticate
 
-st.session_state['root_path'] = os.path.dirname(os.path.abspath(__file__))
-st.session_state['root_url'] = _CONFIG['root']
-st.session_state['questions_chunk_size'] = _CONFIG['questions_chunk_size']
+authenticator = Authenticate(
+    secret_credentials_path='google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri='http://localhost:8501',
+)
 
-if not st.session_state.get('connected'):
-    print("checking cache..")
-    auth.check_cached_sessions(st)
-    auth.auth(st)
+# Check if the user is already authenticated
+authenticator.check_authentification()
 
+# Display the login button if the user is not authenticated
+if not st.session_state.get('connected', False):
+    authorization_url = authenticator.get_authorization_url()
+    st.markdown(f'[Login]({authorization_url})')
+    st.link_button('Login', authorization_url)
+# Display the user information and logout button if the user is authenticated
 else:
-    auth.save_user_and_session(st)
+    st.image(st.session_state['user_info'].get('picture'))
+    st.write(f"Hello, {st.session_state['user_info'].get('name')}")
+    st.write(f"Your email is {st.session_state['user_info'].get('email')}")
+    if st.button('Log out'):
+        authenticator.logout()
 
-    style.render(st)
-    header.header(st)
-    route(st)
 
-    print(st.session_state)
+# st.set_page_config(layout="wide")
+# _CONFIG = json.load(open('config.json'))
+
+# st.session_state['root_path'] = os.path.dirname(os.path.abspath(__file__))
+# st.session_state['root_url'] = _CONFIG['root']
+# st.session_state['questions_chunk_size'] = _CONFIG['questions_chunk_size']
+
+# if not st.session_state.get('connected'):
+#     print("checking cache..")
+#     auth.check_cached_sessions(st)
+#     auth.auth(st)
+
+# else:
+#     auth.save_user_and_session(st)
+
+#     style.render(st)
+#     header.header(st)
+#     route(st)
+
+#     print(st.session_state)
