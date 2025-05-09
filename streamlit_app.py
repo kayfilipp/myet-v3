8e44f4ad55a51fdf51acc96231c193fe7039fa46
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-09 00:07:02 -0700

    favicon

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.set_page_config(
     page_title="MYET",
-    page_icon="✨",
+    page_icon="./assets/favicon-32x32.png",
     initial_sidebar_state="expanded",
+    layout="wide"
 )
 
 # Define app pages
