0b29922220ccb006d3cbe1d23d855a3560f961eb
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-09 01:52:50 -0700

    switch it up i guess

--- main.py
+++ main.py
@@ -10,26 +10,3 @@ if "admin" in user_info['roles']:
   st.write("You are an admin")
 else:
   st.write("You are not an admin")
-
-
-
-# st.set_page_config(layout="wide")
-# _CONFIG = json.load(open('config.json'))
-
-# st.session_state['root_path'] = os.path.dirname(os.path.abspath(__file__))
-# st.session_state['root_url'] = _CONFIG['root']
-# st.session_state['questions_chunk_size'] = _CONFIG['questions_chunk_size']
-
-# if not st.session_state.get('connected'):
-#     print("checking cache..")
-#     auth.check_cached_sessions(st)
-#     auth.auth(st)
-
-# else:
-#     auth.save_user_and_session(st)
-
-#     style.render(st)
-#     header.header(st)
-#     route(st)
-
-#     print(st.session_state)
