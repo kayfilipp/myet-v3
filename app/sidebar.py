64dd7b610d2043131be8e02dad4386a004c24fc7
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-10 16:44:09 -0700

    allow unsafe html

--- app/sidebar.py
+++ app/sidebar.py
@@ -6,6 +6,6 @@ def render(st):
 
     # add a logout button to the sidebar
     with st.sidebar:
-        st.markdown(f"<p style='no-ref'>Logged in with {user.email}<p>")
+        st.markdown(f"<p style='no-ref'>Logged in with {user.email}<p>", unsafe_allow_html=True)
         if st.button("🔓 Logout"):
             st.logout()
