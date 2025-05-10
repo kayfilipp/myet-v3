30ec7210b91f0cdfa6b4873ebbeb6d80c88d64f8
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-10 16:42:16 -0700

    add type annotation

--- app/sidebar.py
+++ app/sidebar.py
@@ -1,9 +1,11 @@
+from models.User import User
+
 def render(st):
 
-    user = st.session_state['User']
+    user: User = st.session_state['User']
 
     # add a logout button to the sidebar
     with st.sidebar:
-        st.caption(f"Logged in with {user['email']}")
+        st.caption(f"Logged in with {user.email}")
         if st.button("🔓 Logout"):
             st.logout()
