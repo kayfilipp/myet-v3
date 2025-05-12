769a5dd21d74af07b8e680debc67b2e99241ee96
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-12 15:58:35 -0700

    parse json

--- models/Assessment.py
+++ models/Assessment.py
@@ -82,7 +82,7 @@ class Assessment:
 
     def save_assessment(self):
         json_results = json.dumps(self.results)
-        query = "insert into assessment(user_id,json_results) select %s,%s"
+        query = "insert into assessment(user_id,json_results) select %s, PARSE_JSON(%s)"
         params = (self.user.id, json_results)
 
         SNOWFLAKE.run_query(
