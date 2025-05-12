d8880e32a63869757873df2111ceee2c5cec977b
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-12 16:53:37 -0700

    grab assessment by id

--- app/quiz.py
+++ app/quiz.py
@@ -68,9 +68,7 @@ assessment_result_id = st.query_params.get('assesment_id')
 # ***************************************************************IF ASSESSMENT ID IS PROVIDED*************************************************************************************** #
 if assessment_result_id:
     profile: Profile = st.session_state['profile']
-    profile.get_assessment_history()
-    
-    assessment = next((assessment for assessment in profile.assessments if assessment.id == assessment_result_id), {})
+    assessment = profile.get_assessment(assessment_result_id)
     st.session_state['assessment'] = Assessment(
         user=user,
         saved=True,
--- models/Profile.py
+++ models/Profile.py
@@ -8,9 +8,27 @@ class Profile:
 
     def __init__(self, user: User):
         self.user = user 
-        self.assessments: list[dict] = None
+        self.assessments: list[dict] = []
 
     def get_assessment_history(self):
+
         query="select id, created_date, json_results, is_public from assessment where user_id = %s"
         params = [self.user.id]
         self.assessments = SNOWFLAKE.run_query(query=query, params=params, return_=True, commit=False, as_dict=True)
+        return 
+
+    def get_assessment(self, id):
+
+        # check locally 
+        if self.assessments != []:
+            search = next((assessment for assessment in self.assessments if assessment.id == id))
+            if search:
+                return search 
+            
+        # then cloud 
+        query="select id, created_date, json_results, is_public from assessment where user_id = %s and id = %s"
+        params = [self.user.id, id]
+        assessment =  SNOWFLAKE.run_query(query=query, params=params, return_=True, commit=False, as_dict=True)
+
+        # add assessment to profile 
+        self.assessments += assessment 
