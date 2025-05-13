c15ce6dc17ac8ec75ac8c0ef88a47ddfcd7a36b6
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-12 22:18:02 -0700

    ref ksuid for score

--- models/AssessmentScore.py
+++ models/AssessmentScore.py
@@ -44,7 +44,7 @@ class AssessmentScore:
 
 
     async def save(self):
-        query = "insert into assessment(id, user_id,json_results) select %s, %s, PARSE_JSON(%s)"
+        query = "insert into assessment(ksuid, user_id,json_results) select %s, %s, PARSE_JSON(%s)"
         params = (self.id, self.user.id, json.dumps(self.results))
 
         SNOWFLAKE.run_query(
