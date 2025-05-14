101dd2bc3d77fed7e601dabb633dfcc37a900393
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 03:54:13 -0700

    no rly wheres the expander:

--- app/app.py
+++ app/app.py
@@ -51,9 +51,8 @@ if user.id:
     if not user.shared_scores:
         user.get_shared_scores()
 
-    st.write(len(user.shared_scores))
-
     with st.expander("Scores Shared With You", expanded=True):
+        st.write(f"{len(user.shared_scores)} shared with you.")
         for score in user.shared_scores:
 
             if st.button("View", key=score.id):
