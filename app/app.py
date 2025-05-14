a1437f1a6c3c4c584b14b67ead9abaa04ecb2835
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 01:03:18 -0700

    smaller col gap

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ if user.id:
 
     for score in user.assessment_scores:
         st.caption(f'Taken on {score.created_date}')
-        c = st.columns([8,2])
+        c = st.columns([4,2])
         with c[0]:
             st.dataframe([score.results])
         with c[1]:
