60aede6279bece343d584cfc7cf4824cd36ebf4f
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 19:09:04 -0700

    move subheader out of cols

--- components/results.py
+++ components/results.py
@@ -6,10 +6,9 @@ def render(st: streamlit, assessment_score, enable_management=True):
 
     results = assessment_score.results
 
+    st.subheader("You are: The Worker Bee")
     c = st.columns([2,2,8])
-
     with c[0]:
-        st.subheader("You are: The Worker Bee")
         st.image("./assets/workerbee.png")
     with c[1]:
         st.write("You do stuff!")
