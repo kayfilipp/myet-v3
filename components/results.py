5e35dd650e8e05ab247c36006676825b3d725385
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 19:08:24 -0700

    crop image

--- components/results.py
+++ components/results.py
@@ -6,8 +6,13 @@ def render(st: streamlit, assessment_score, enable_management=True):
 
     results = assessment_score.results
 
-    st.subheader("You are: The Worker Bee")
-    st.image("./assets/workerbee.png")
+    c = st.columns([2,2,8])
+
+    with c[0]:
+        st.subheader("You are: The Worker Bee")
+        st.image("./assets/workerbee.png")
+    with c[1]:
+        st.write("You do stuff!")
 
     c = st.columns([1.5,2.5,6])
 
