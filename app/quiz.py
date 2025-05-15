ab34bfe1281daf9215010cbd6272b9f5896f6ab0
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 17:43:44 -0700

    prompt before cols

--- app/quiz.py
+++ app/quiz.py
@@ -108,9 +108,9 @@ else:
 
     num_questions = len(assessment.questions)
 
-    c = st.columns([1,1,10])
+    st.text(f"This assessment will consist of {num_questions} questions. Feel free to explore our website and take breaks - your progress will be saved.") 
 
-    st.text(f"This assessment will consist of {num_questions} questions. Feel free to explore our website and take breaks - your progress will be saved.")        
+    c = st.columns([1,1,10])       
     c[0].button("Start", on_click=assessment.start)
 
     # add a demo button because I'm lazy.
