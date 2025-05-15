464f44a36f7d808350e5e77fc969406ff66de28e
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 19:12:57 -0700

    meh

--- components/results.py
+++ components/results.py
@@ -7,27 +7,23 @@ def render(st: streamlit, assessment_score, enable_management=True):
     results = assessment_score.results
 
     st.subheader("You are: The Worker Bee")
-    c = st.columns([2,2,8], gap='medium')
+    c = st.columns([2,2.5,2, 8], gap='medium')
     with c[0]:
         st.image("./assets/workerbee.png")
+
     with c[1]:
-        st.write("You do stuff!")
-
-    c = st.columns([1.5,2.5,6])
-
-    with c[0]:
         st.subheader("Your Scores")
         st.caption("Here's how you did.")
         st.dataframe(results, use_container_width=True, column_config={"value": 'score'})
 
-    with c[1]:
+    with c[2]:
         st.subheader("Visual Breakdown")
         st.caption("Who doesn't like pictures?")
         score_chart.render(st, results)
 
-    with c[2]:
-        st.subheader("Understanding your Results")
-        trait_descriptions.render(st)
+
+    st.subheader("Understanding your Results")
+    trait_descriptions.render(st)
 
     st.divider()
 
