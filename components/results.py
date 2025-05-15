99cf6983fe3c52ff6da0f73b2a7780aaa86a9ca9
Filipp Krasovsky <36943200+kayfilipp@users.noreply.github.com>
2025-05-14 19:14:39 -0700

    fmt

--- components/results.py
+++ components/results.py
@@ -8,7 +8,8 @@ def render(st: streamlit, assessment_score, enable_management=True):
 
     c = st.columns([2,2.5,2, 8], gap='medium')
     with c[0]:
-        st.subheader("You are: The Worker Bee")
+        st.caption("You are...")
+        st.subheader("The Worker Bee")
         st.image("./assets/workerbee.png")
 
     with c[1]:
